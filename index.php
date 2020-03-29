f213a6577e260722d1f9c82e6df5d2948ba22aee
RishiOsmar <60679732+RishiOsmar@users.noreply.github.com>
2020-03-29 13:34:16 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>SI Control Escolar</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+<style>
+body {
+  background-image: url('fondo.jpg');
+  background-attachment: fixed;
+  background-size: cover;
+}
+
+a:link, a:visited {
+  background-color: #f44336;
+  color: white;
+  padding: 15px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+
+a:hover, a:active {
+  background-color: red;
+}
+
+</style> 
+
+<center> 
+
+<h1> Control Escolar </h1>
+<br>
+<br>
+<a href="FormALUMNOS.php">Control de Alumnos <br> </a> 
+
+<a href="form_carreras.php">Control de Carreras<br> </a> 
+
+<a href="ForMAESTROS.php">Control de Maestros<br> </a> 
+
+<a href="form_grupos.php">Control de Grupos<br> </a> 
+
+<a href="form_materias.php">Control de Materias<br> </a> 
+
+</center>
+
+
+</body>
+</html>
